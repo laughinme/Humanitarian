6a62d5ba8b1f2a9b8cf40fa70f6b8caa55a6c3e3
laughinme <wixw1cky7@gmail.com>
2023-11-27 01:36:08 +0700

    updates
    -v1 endpoint
    -status_codes added

--- FastApi/chichis.py
+++ FastApi/chichis.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+# from pydantic import BaseModel
 import sqlite3
 import random
 import string
-from typing import Optional
+# from typing import Optional
 import uvicorn
 
 
@@ -17,9 +17,6 @@ app = FastAPI(
 # async def root():
 #     return {"message": "Hello World"}
 
-# @app.get("/hello")
-# def hello():
-#     return "Hello World"
 
 def generate_code():
     count = 1
@@ -31,7 +28,7 @@ def generate_code():
             count = cur.fetchone()[0]
         return code
     
-@app.post("/users/register")
+@app.post("/v1/users/register")
 async def register(username: str, password: str, age: int=None, school: str=None, name: str=None):
     with sqlite3.connect('users.db') as conn:
         cur = conn.cursor()
@@ -47,49 +44,50 @@ async def register(username: str, password: str, age: int=None, school: str=None
             return JSONResponse(status_code=status.HTTP_302_FOUND, content={"message": "Такой пользователь существует!", "username" : username})
     
 
-@app.post("/users/login")
-async def login(username: str, password: str, name: str = None):
+@app.post("/v1/users/login")
+async def login(username: str, password: str):
     with sqlite3.connect('users.db') as conn:
         cur = conn.cursor()
         cur.execute(f'SELECT password FROM users WHERE username = "{username}"')
         dbpassword = cur.fetchone()
         if not dbpassword:
-            code = generate_code()
-            cur.execute(f'INSERT INTO users (username, name, password, code) VALUES (?, ?, ?, ?)', (username, name, password, code))
-            conn.commit()
-            return {"message": "Успешная регистрация!", "code": "200", "auth_code": str(code), "role": "user"}
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": "Такого пользователя не существует!"})
         else:
             dbpassword = dbpassword[0]
             if dbpassword == password:
                 cur.execute('SELECT role, code FROM users WHERE username = ?', (username,))
                 role, code = cur.fetchone()
-                return {"message": "Успешный вход!", "code": "200", "auth_code": str(code), "role": role}
+                return {"message": "Успешный вход!", "auth_code": str(code), "role": role}
             
             elif dbpassword != password:
-                return {"message": "Неверный пароль!", "code": "401"}
+                return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={"message": "Неверный пароль!"})
         
 
-@app.get("/users/get")
+@app.get("/v1/users/get")
 async def get_user(code: int):
     with sqlite3.connect('users.db') as conn:
         cur = conn.cursor()
         cur.execute(f'SELECT username FROM users WHERE code = "{code}"')
         username = cur.fetchone()
         if not username:
-            return {"message": "Пользователь не найден!", "code": "404"}
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": "Такого пользователя не существует!"})
         else:
-            cur.execute(f'SELECT name FROM users WHERE code = "{code}"')
-            name = cur.fetchone()
-            return {"message": "Пользователь найден!", "code": "200", "username": username[0], "name": name[0] if name else name}
+            cur.execute(f'SELECT name, age, school FROM users WHERE code = "{code}"')
+            name, age, school = cur.fetchone()
+            return {"message": "Пользователь найден!", "username": username[0], "name": name, "age" : age, "school" : school}
 
 
-@app.post("/users/grant")
+@app.post("/v1/users/grant")
 async def grant_user(code: int):
     with sqlite3.connect('users.db') as conn:
         cur = conn.cursor()
-        cur.execute(f'UPDATE users SET role = "checker" WHERE code = "{code}"')
-        conn.commit()
-        return {"message": "Роль пользователя успешно изменена!", "code": "200"}
+        cur.execute(f'SELECT COUNT(*) FROM users WHERE code = "{code}"')
+        if cur.fetchone():
+            cur.execute(f'UPDATE users SET role = "checker" WHERE code = "{code}"')
+            conn.commit()
+            return {"message": "Роль пользователя успешно изменена!"}
+        else:
+            return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": "Такого пользователя не существует!"})
     
 
-# uvicorn.run(app)
+uvicorn.run(app, host='0.0.0.0', port=8677)
--- FastApi/test.py
+++ FastApi/test.py
@@ -1,7 +1,7 @@
 import requests
 url = 'http://127.0.0.1:8000/users/register'
-request = requests.post(url, params={"name" : "negro", "ochkis" : "egor"})
+request = requests.post(url, params={"username" : "admin", "password" : "admin", "name" : "admin", "age" : 10, "school" : "ELNSTU"})
 
 print(request.url)
 print(request.text)
-print(request.json())
+print(request.status_code)
